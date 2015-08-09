d94bc5e7b1a96353f790decf0486b6d705c3cf05
Jason Rice <jsnrice@gmail.com>
2015-08-09 00:43:19 -0400

    Added planetary entity table and data set pertaining to our moon.

--- sql/NewHorizons DDL.sql
+++ sql/NewHorizons DDL.sql
@@ -1,5 +1,11 @@
---drop table NH_USERS;
+--------------------------------------------------------------------------------
+-- This file is used as a reference point and contains the default SQL for
+-- loading into db4free.
+--------------------------------------------------------------------------------
 
+-- Table Creation:
+
+-- DROP TABLE NH_USERS;
 create table NH_USERS (
     USER_ID MEDIUMINT NOT NULL AUTO_INCREMENT,
     USERNAME CHAR(200),
@@ -12,17 +18,50 @@ create table NH_USERS (
 )
 
 -- Discoveries Page
+-- DROP TABLE PLANETARY_ENTITY
 CREATE TABLE PLANETARY_ENTITY (
     PLANETARY_ENTITY_ID MEDIUMINT NOT NULL AUTO_INCREMENT,
     ISPLANET BIT(0),
     ISMOON BIT(0),
-    NAME CHAR(64),
     ORBITING_MOON_SURVEY CHAR(200),
     ORBITING_MOON_COUNT TINYINT,
     RING_DATA   CHAR(200),
     IMAGE BLOB,
-    ENTRY_LAST_UPDATED DEFAULT CURRENT_TIMESTAMP,
     NAME CHAR(64) NOT NULL UNIQUE,    
-    PRIMARY KEY (PLANETARY_ENTITY_ID),
+    DATA TEXT,
+    PRIMARY KEY (PLANETARY_ENTITY_ID)
 )
 
+-- Table Truncation and Insertions:
+
+TRUNCATE TABLE PLANETARY_ENTITY
+
+-- Earth's Moon
+-- Note the LOAD_FILE doesn't seem to work so to get around this just open up 
+-- the db4free newhorizons db in Services and manually read image file in.
+INSERT INTO PLANETARY_ENTITY(NAME, ISMOON, DATA, ORBITING_MOON_COUNT, IMAGE)
+    VALUES('The Moon', 1, 
+'Lunar orbital data: 
+Semimajor axis (106 km)             0.3844                 
+Perigee (106 km)*                   0.3633
+Apogee (106 km)*                    0.4055
+Revolution period (days)            27.3217            
+Synodic period (days)               29.53               
+Mean orbital velocity (km/s)        1.022                  
+Max. orbital velocity (km/s)        1.076
+Min. orbital velocity (km/s)        0.964
+Inclination to ecliptic (deg)       5.145
+Inclination to equator (deg)        18.28 - 28.58                    
+Orbit eccentricity                  0.0549              
+Sidereal rotation period (hrs)      655.728                
+Obliquity to orbit (deg)            6.68                   
+Recession rate from Earth (cm/yr)   3.8
+
+Mean values at opposition from Earth
+        Distance from Earth (equator, km)   378,000    
+        Apparent diameter (seconds of arc)     1896
+        Apparent visual magnitude               -12.74     
+
+* These represent mean apogee and perigee for the lunar orbit.  
+The orbit changes over the course of the year so the distance 
+from the Moon to Earth roughly ranges from 357,000 km to 407,000 km.', 0, LOAD_FILE('/home/jsnrice/Desktop/themoon.jpg'))
